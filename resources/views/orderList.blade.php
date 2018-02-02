df43d348b50125b9893b2d0e0d900d4b35f793d6
Ping Xin <xinpingxian@gmail.com>
2018-02-02 16:34:34 -0600

    fix problem of modal is not a function

--- resources/views/orderList.blade.php
+++ resources/views/orderList.blade.php
@@ -14,7 +14,6 @@
 <script src="js/edit.js"></script>
 
 <link rel="stylesheet" href="//apps.bdimg.com/libs/jqueryui/1.10.4/css/jquery-ui.min.css">
-<script src="//apps.bdimg.com/libs/jquery/1.10.2/jquery.min.js"></script>
 <script src="//apps.bdimg.com/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
 <script>
     $(function() {
